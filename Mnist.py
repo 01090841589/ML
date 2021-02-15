c3127ed80d33733db803829c7e2cac21b8518ddb
01090841589 <chanchanhwan@naver.com>
2021-02-15 18:17:06 +0900

    20210215

--- Mnist.py
+++ Mnist.py
@@ -12,3 +12,10 @@ import collections
 %matplotlib inline
 import matplotlib.pyplot as plt
 from cirq.contrib.svg import SVGCircuit
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train, x_test = x_train[..., np.newaxis]/255.0, x_test[..., np.newaxis]/255.0
+
+print("Number of original training examples:", len(x_train))
+print("Number of original test examples:", len(x_test))
