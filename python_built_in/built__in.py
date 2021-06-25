f4cbfb36654b2c0d95c45e05c42a2b8100eaa3d1
01090841589 <chanchanhwan@naver.com>
2021-06-26 00:42:50 +0900

    conda 기본설정

---
+++ python_built_in/built__in.py
@@ -0,0 +1,18 @@
+
+# abs() 숫자의 절댓값
+print(abs(1))
+print(abs(-1))
+
+# all() 모든 요소가 참이면 True
+print(all([1, 1, 1]))
+print(all([1, 0, 1]))
+
+# any()
+print(any([0, 0, 0]))
+print(any([1, 0, 1]))
+
+# ascii()
+print(ascii("2")) 
+
+# bin()
+print(bin(3))
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Tensorflow 2.0
+
+## conda 가상환경 설정
+
+### 파이썬 3.8 가상환경 설정
+
+conda create -n py3.8 python=3.8
+
+pip install matplotlib
+
+* conda activate가 실행되지 않으면
+
+* source ~/anaconda3/etc/profile.d/conda.sh
+
+  
