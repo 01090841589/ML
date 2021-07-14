cadcaad8486f7e50d5520c4f14a8659839612375
01090841589 <chanchanhwan@naver.com>
2021-07-14 16:36:22 +0900

    20210714

--- Tensorflow_2.0_/fashion.py
+++ Tensorflow_2.0_/fashion.py
@@ -20,3 +20,33 @@ print(len(train_labels)) # 레이블 수
 print(train_labels) # 레이블 분포
 print(test_images.shape) # 테스트 이미지수, 픽셀
 print(len(test_labels)) # 이미지 레이블 수
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0 # 값의 범위를 0~1사이로 조정
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
