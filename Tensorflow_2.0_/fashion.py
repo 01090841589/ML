1fecc3a23b89a66f440a4130c2b08dffabf816b5
01090841589 <chanchanhwan@naver.com>
2021-07-16 23:11:48 +0900

    20210716

--- Tensorflow_2.0_/fashion.py
+++ Tensorflow_2.0_/fashion.py
@@ -106,14 +106,6 @@ plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 plt.show()
 
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  test_labels)
-plt.show()
-
 i = 50
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
